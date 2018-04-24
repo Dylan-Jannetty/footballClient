79c9e5adc09d509c4f56e5a73f1d70420124a6f3
dylan-jannetty <djannetty29@gmail.com>
2018-04-24 15:57:08 -0400

    for deploy added heroku URL in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://peaceful-journey-18684.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
