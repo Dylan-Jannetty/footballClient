1bffd6bc3ddb5ff495fa6e586d440abb734239b6
dylan-jannetty <djannetty29@gmail.com>
2018-04-26 19:47:36 -0400

    took stuff out took stuff out

--- assets/scripts/team/teamevents.js
+++ assets/scripts/team/teamevents.js
@@ -39,15 +39,7 @@ const addHandlers = () => {
   $('#create-team').on('submit', onCreateTeam)
   $('#update-team').on('submit', onUpdateTeam)
   $('body').on('submit', '.delete-team', onDeleteTeam)
-  $('.deleteTeam').on('click', () => {
-    $('.Team').toggle()
-    $('.Team').show()
-  })
   $('#get-teams').on('click', onGetTeams)
-  $('#hide-teams').on('click', () => {
-    $('.Team').toggle()
-    $('.hide-teams').addClass('hidden')
-  })
   $('.modal').on('hidden.bs.modal', function (e) {
     $('input[type="email"], input[type="password"], input[type="text"], input[type="date"], input[type="number"], textarea').val('')
   })
